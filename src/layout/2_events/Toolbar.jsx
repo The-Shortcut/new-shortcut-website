6a0815c5721467c72c86dc4d8a2a5702f9ce1aaf
Arifa <arifabegam.mujawar@gmail.com>
2020-09-14 18:44:08 +0300

    Sticky nav bar, rephrasing changes

--- src/layout/2_events/Toolbar.jsx
+++ src/layout/2_events/Toolbar.jsx
@@ -6,23 +6,27 @@ import css from "./styles.module.scss";
 // Children
 import Search from "./Search";
 
-const Toolbar = ({setSearch}) => {
+const Toolbar = ({ setSearch }) => {
   return (
     <div className={css.toolbar}>
       <ul>
         <li>
           <Search setSearch={setSearch} />
         </li>
-        <li onClick={() =>setSearch('workshop')}>Workshops</li>
-        <li onClick={() =>setSearch('training')}>Trainings</li>
-        <li onClick={() =>setSearch('certification')}>Certifications</li>
-        <li onClick={() => setSearch('club')}>Clubs</li>
       </ul>
       <ul>
-        <li onClick={() => setSearch('my startup debut')}>#mystartupdebut</li>
-        <li onClick={() => setSearch('python')}>#python</li>
-        <li onClick={() => setSearch('talent heist')}>#talentheist</li>
-        <li onClick={() => setSearch('school of startup')}>#schoolofstartups</li>
+        <li onClick={() => setSearch("workshop")}>Workshops</li>
+        <li onClick={() => setSearch("training")}>Trainings</li>
+        <li onClick={() => setSearch("certification")}>Certifications</li>
+        <li onClick={() => setSearch("club")}>Clubs</li>
+      </ul>
+      <ul>
+        <li onClick={() => setSearch("my startup debut")}>#mystartupdebut</li>
+        <li onClick={() => setSearch("python")}>#python</li>
+        <li onClick={() => setSearch("talent heist")}>#talentheist</li>
+        <li onClick={() => setSearch("school of startup")}>
+          #schoolofstartups
+        </li>
       </ul>
     </div>
   );
