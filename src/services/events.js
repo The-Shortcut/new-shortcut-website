eeb406ad1c35575dd2c675b57ce6158740ff6ec7
mostafaIn <mostafa.hazareh@theshortcut.org>
2020-09-29 17:32:38 +0300

    Events are filtered out drafts

--- src/services/events.js
+++ src/services/events.js
@@ -3,8 +3,11 @@ import axios from 'axios'
 let API_TOKEN = `${process.env.REACT_APP_EVENT_API_TOKEN}`
 
 const getEvents = async() => {
-    const response = await axios.get(`https://www.eventbriteapi.com/v3/organizations/171778300477/events/?order_by=start_desc&token=${API_TOKEN}`)
-    return response.data.events
+    const eventsResponse = await axios.get(`https://www.eventbriteapi.com/v3/organizations/171778300477/events/?order_by=start_desc&token=${API_TOKEN}`)
+    const draftsResponse = await axios.get(`https://www.eventbriteapi.com/v3/organizations/171778300477/events/?order_by=start_desc&status=draft&token=${API_TOKEN}`)
+    const response = eventsResponse.data.events.filter((event) => !draftsResponse.data.events.find(({ id }) => event.id === id))
+    
+    return response
 }
 
 
