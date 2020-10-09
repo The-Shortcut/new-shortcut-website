a13f713a2e0c95d53ba9575cbff524a1722326f4
Arifa <arifabegam.mujawar@gmail.com>
2020-10-09 12:45:45 +0300

    submit button outside, search input height problem solved

--- src/layout/1_about/8_talent_pool/Talent.jsx
+++ src/layout/1_about/8_talent_pool/Talent.jsx
@@ -35,13 +35,13 @@ const Talent = ({ data }) => {
           <h4 className={css.name}>{data.name}</h4>
           <p>{data.hashtags}</p>
           <div className={css.links}>
-            <a href={`mailto:${data.email}`}>
+            <a href={`mailto:${data.email}`} target="_blank">
               <img
                 src="https://theshortcut.org/wp-content/uploads/2019/10/email-icon-01.png"
                 alt="mail-icon"
               ></img>
             </a>
-            <a href={data.linkedInProf}>
+            <a href={data.linkedInProf} target="_blank">
               {" "}
               <img
                 src="https://theshortcut.org/wp-content/uploads/2019/10/linkedin-2.png"
@@ -51,35 +51,39 @@ const Talent = ({ data }) => {
           </div>
         </div>
       </div>
-      <ReactModal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <div className={css.modalContainer}>
-          <button onClick={closeModal} className={css.modalClose}>
-            <MdClose />
-          </button>
-          <h4 className={css.modalName}>{data.name}</h4>
-          <div className={css.modalLinks}>
-            <a href={`mailto: ${data.email}`}>
+      <div className={css.modal}>
+        <ReactModal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div className={css.modalContainer}>
+            <button onClick={closeModal} className={css.modalClose}>
+              <MdClose />
+            </button>
+            <h4 className={css.modalName}>{data.name}</h4>
+            <div className={css.modalLinks}>
+              {/* <a href={`mailto: ${data.email}`} target="_blank">
               <img
                 src="https://theshortcut.org/wp-content/uploads/2019/10/email-icon-01.png"
                 alt="mail-icon"
+                className={css.email}
               ></img>
             </a>
-            <a href={data.linkedInProf}>
+            <a href={data.linkedInProf} target="_blank">
               <img
                 src="https://theshortcut.org/wp-content/uploads/2019/10/linkedin-2.png"
                 alt="linkedin-icon"
+                className={css.linkedin}
               />
-            </a>
-          </div>
+            </a> */}
+            </div>
 
-          <div className={css.modalContent}>{striptags(data.content)}</div>
-        </div>
-      </ReactModal>
+            <div className={css.modalContent}>{striptags(data.content)}</div>
+          </div>
+        </ReactModal>
+      </div>
     </div>
   );
 };
