303d9e7bc981e3b915b8e8f608cbf0044611808a
MostafaIn <mostafa.hazareh@theshortcut.org>
2021-03-12 02:39:38 +0200

    the events are sorted by start_date, modified the search events tags and filter the completed events just display by search tag

--- src/actions/eventActions.js
+++ src/actions/eventActions.js
@@ -10,7 +10,6 @@ import eventServices from "../services/events";
 
 export const allEvents = () => async (dispatch) => {
   const events = await eventServices.getEvents();
-
   dispatch({
     type: INIT_EVENTS,
     payload: events,
